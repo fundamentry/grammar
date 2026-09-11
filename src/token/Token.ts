6a4a1cc23b7cdfe1ba1893f8a0568a7c5e70e7d7
Alexander Zakharov <alexander.zakharov@fundamentry.org>
2026-09-11 11:19:02 +0200

    feat: add 'Token.Definition' type

--- src/token/Token.ts
+++ src/token/Token.ts
@@ -1,3 +1,10 @@
+export namespace Token {
+  export interface Definition<Element, T extends Token> {
+    test(element: Element): boolean;
+    create(element: Element): T;
+  }
+}
+
 export abstract class Token<Type extends string = string, Value = unknown> {
   readonly #type: Type;
 
